f904509d70b038ebe9529286d1317b1f3617e9f5
695954085 <695954085@qq.com>
2018-12-17 18:03:10 +0800

    feat: 增加joi，登录注册参数验证

---
+++ src/views/login/register.schema.js
@@ -0,0 +1,10 @@
+import Joi from 'joi'
+
+const registerSchema = Joi.object().keys({
+  usercard: Joi.string().regex(/^A\d{4}$/),
+  username: Joi.string().regex(/^\w{5,10}$/),
+  password: Joi.string().regex(/^\w{8}$/),
+  confirmPassword: Joi.string().regex(/^\w{8}$/)
+})
+
+export default registerSchema
